4cff4a32b74ae6fe94008a32ab059824486362ec
wu1274704958 <1274704958@qq.com>
2020-02-09 13:31:14 +0800

    l.

--- res/lighting_4/base_fg.glsl
+++ res/lighting_4/base_fg.glsl
@@ -1,14 +1,18 @@
 #version 450 core
 
+struct DirctLight{
+    vec3 color;
+    vec3 dir;
+};
+
 in vec3 oNormal; 
 in vec3 oVpos; 
 in vec2 oUv;
 in vec3 o_pl_pos;
 out vec4 color;
 
-layout (std140,binding = 0) uniform DirctLight{
-    vec3 light_color;
-    vec3 light_dir;
+layout (std140,binding = 0) uniform DL{
+    DirctLight dirct_light;
 };
 
 uniform sampler2D diffuseTex;
@@ -27,19 +31,19 @@ void main()
 { 
     vec3 obj_color = texture(diffuseTex, oUv).rgb;
     
-    vec3 light_dirction = -normalize(light_dir);
+    vec3 light_dirction = -normalize(dirct_light.dir);
 
     vec3 view_dir = normalize(view_pos - oVpos);
 
-    vec3 ambient = light_color * ambient_strength;
+    vec3 ambient = dirct_light.color * ambient_strength;
 
-    vec3 diffuse = (obj_color * light_color) * max(dot(light_dirction,oNormal),0.0f);
+    vec3 diffuse = (obj_color * dirct_light.color) * max(dot(light_dirction,oNormal),0.0f);
 
     vec3 light_reflect = reflect(-light_dirction,oNormal);
 
     float spec = pow(max(dot(view_dir,light_reflect),0.0f),shininess);
 
-    vec3 specular = specular_strength * spec * (light_color * texture(specularTex,oUv).rgb);
+    vec3 specular = specular_strength * spec * (dirct_light.color * texture(specularTex,oUv).rgb);
 
     //point light calc
     vec3 pl_dir = normalize(o_pl_pos - oVpos);
