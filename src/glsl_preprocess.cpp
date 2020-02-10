32f2b99be1b2331cc72c4fdc5ab90091ede0f2f2
wu1274704958 <1274704958@qq.com>
2020-02-10 14:08:06 +0800

    l.

--- src/glsl_preprocess.cpp
+++ src/glsl_preprocess.cpp
@@ -19,19 +19,19 @@ namespace gld::glsl{
     std::optional<std::string> IncludePreprocess::handle(std::filesystem::path path,const std::vector<token::Token>& ts,int b,int e,std::function<std::string(std::filesystem::path,std::string&&)> process_f)
     {
         std::string ps = path.string();
-        if(IncludeCatche::get_instance()->has(ps))
+        std::filesystem::path in_path = path.parent_path();
+        in_path /= ts[b + 1].body.c_str();
+        std::string in_ps = in_path.string();
+        if(IncludeCatche::get_instance()->has(in_ps))
         {
-            return std::optional<std::string>( IncludeCatche::get_instance()->map[ps] );
+            return std::optional<std::string>( IncludeCatche::get_instance()->map[in_ps] );
         }else{
-            std::filesystem::path in_path = path.parent_path();
-            in_path /= ts[b + 1].body.c_str();
             if(fs::exists(in_path))
             {
                 auto res = LoadText::load(in_path);
                 if(res)
                 {
                     std::string process_res = process_f(in_path,std::move(*res));
-                    std::string in_ps = in_path.string();
                     IncludeCatche::get_instance()->set(in_ps,process_res);
                     return std::optional<std::string>( std::move(process_res) );
                 }else{
