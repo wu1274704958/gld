ba1483c424acd3ad407f5e1d92a6b528382151bb
wws <1274704958@qq.com>
2020-02-28 13:21:19 +0800

    data mgr

--- include/data_comm.hpp
+++ include/data_comm.hpp
@@ -1,9 +1,96 @@
 #pragma once
 
+#include <res_comm.hpp>
+#include <comm.hpp>
+
 namespace gld{
     enum class DataType{
-        Program = 0x0,
+        Program = 0x100,
         Texture,
         Scene
+    };
+
+    namespace data_ck{
+
+#ifndef PF_ANDROID
+    template <class T,typename ...Args>											
+    using has_load_func_t = decltype(T::load(std::declval<Args>()...));
+#else
+    template <class T,typename ...Args>
+    using has_load_func_t = decltype(T::load(std::declval<AndroidCxtPtrTy>(),std::declval<Args>()...));
+#endif
+    template <typename T,typename ...Args>
+    using has_load_func_vt = wws::is_detected<has_load_func_t,T,Args...>;
+
+#ifndef PF_ANDROID
+    template <class T>
+    using has_load_func2_t = decltype(T::load());
+#else
+    template <class T>
+    using has_load_func2_t = decltype(T::load(std::declval<AndroidCxtPtrTy>()));
+#endif
+    template <typename T>
+    using has_load_func2_vt = wws::is_detected<has_load_func2_t, T>;
+
+#ifndef PF_ANDROID
+    template <class T,class ... Args>
+    using has_load_func3_t = decltype(T::load(std::declval<std::tuple<Args...>>()));
+#else
+    template <class T,class ... Args>
+    using has_load_func3_t = decltype(T::load(std::declval<AndroidCxtPtrTy>(),std::declval<std::tuple<Args...>>()));
+#endif
+    template <typename T,class ...Args>
+    using has_load_func3_vt = wws::is_detected<has_load_func2_t, T,Args ...>;
+
+    template <class T>											
+    using has_ret_type_t = typename T::RetTy;
+
+    template <typename T>
+    using has_ret_type_vt = wws::is_detected<has_ret_type_t,T>;
+
+    template <class T>											
+    using has_args_type_t = typename T::ArgsTy;
+
+    template <typename T>
+    using has_args_type_vt = wws::is_detected<has_args_type_t,T>;
+
+    template <class T,class Args>											
+    using has_format_args_func_t = decltype(T::format_args(std::declval<Args>()));
+
+    template <typename T,typename Args>
+    using has_format_args_func_vt = wws::is_detected<has_format_args_func_t,T,Args>;
+
+    template <class T>											
+    using has_default_args_func_t = decltype(T::default_args());
+
+    template <typename T>
+    using has_default_args_func_vt = wws::is_detected<has_default_args_func_t,T>;
+
+    template <class T,class Args>											
+    using has_key_from_args_func_t = decltype(T::key_from_args(std::declval<Args>()));
+
+    template <typename T,typename Args>
+    using has_key_from_args_func_vt = wws::is_detected<has_key_from_args_func_t,T,Args>;
+
+    }
+
+
+    template<DataType ty,typename T>
+    struct DataLoadPlugTy
+    {
+        constexpr static size_t res_type = static_cast<size_t>(ty);
+        using type = T;
+
+        static_assert(data_ck::has_ret_type_vt<T>::value,"this type must has RetTy!!!");
+        static_assert(data_ck::has_args_type_vt<T>::value,"this type must has ArgsTy!!!");
+        
+        
+        using Ret = typename T::RetTy;
+        using Args = typename T::ArgsTy;
+        static_assert(
+            data_ck::has_load_func_vt<T,Args>::value || (data_ck::has_load_func2_vt<T>::value && std::is_same_v<Args,void>),
+            "this type must has load func!!!");
+
+        static_assert(data_ck::has_key_from_args_func_vt<T,Args>::value,"this type must has key_from_args function!!!");
     };
 }
--- include/data_mgr.hpp
+++ include/data_mgr.hpp
@@ -1,9 +1,124 @@
 #pragma once
 
-
+#include <data_comm.hpp>
 
 
 namespace gld
 {
-    
+    template<typename ...Plugs>
+    class DataMgr{
+    public:
+
+        template<DataType Rt>
+        auto
+            load(typename MapResPlug<static_cast<size_t>(Rt),Plugs...>::type::ArgsTy args)
+            ->typename MapResPlug<static_cast<size_t>(Rt),Plugs...>::type::RetTy
+        {
+            using Ty = typename MapResPlug<static_cast<size_t>(Rt),Plugs...>::type;
+            using ARGS_T = typename Ty::ArgsTy;
+            using RET_T = typename Ty::RetTy;
+
+            auto key = Ty::key_from_args(args);
+
+            if(ResCacheMgr<Plugs...>::instance()->template has<static_cast<size_t>(Rt)>(key))
+            {
+                return ResCacheMgr<Plugs...>::instance()->template get<static_cast<size_t>(Rt)>(key);
+            }
+#ifndef PF_ANDROID
+            auto [success,res] = Ty::load(std::forward<typename Ty::ArgsTy>(args));
+#else
+            auto [success,res] = Ty::load(mgr,std::forward<typename Ty::ArgsTy>(args));
+#endif
+            if(success)
+                ResCacheMgr<Plugs...>::instance()->template cache<static_cast<size_t>(Rt)>(key,res);
+            return res;
+        }
+
+        template<DataType Rt>
+        auto load()
+            ->typename MapResPlug<static_cast<size_t>(Rt), Plugs...>::type::RetTy
+        {
+
+            static_assert(data_ck::has_default_args_func_vt<typename MapResPlug<static_cast<size_t>(Rt), Plugs...>::type>::value,
+                "Load plug args type must has default_args function!!!");
+
+            using Ty = typename MapResPlug<static_cast<size_t>(Rt), Plugs...>::type;
+            using ARGS_T = typename Ty::ArgsTy;
+            using RET_T = typename Ty::RetTy;
+
+            return load<Rt>(Ty::default_args());
+        }
+
+        template<DataType Rt,typename ...Args>
+        decltype(auto)
+            load(Args&&... args)
+        {
+            using Ty = typename MapResPlug<static_cast<size_t>(Rt),Plugs...>::type;
+            using ARGS_T = typename Ty::ArgsTy;
+            using RET_T = typename Ty::RetTy;
+
+            static_assert(data_ck::has_load_func3_vt<Ty,Args...>::value,"This load plug not has tuple load function!!!");
+
+            load(std::make_tuple(std::forward<Args>(args)...));
+        }
+
+        template<DataType Rt>
+        decltype(auto) rm_cache(typename MapResPlug<static_cast<size_t>(Rt),Plugs...>::type::ArgsTy args)
+        {
+            using Ty = typename MapResPlug<static_cast<size_t>(Rt), Plugs...>::type;
+
+            auto key = Ty::key_from_args(args);
+
+            return ResCacheMgr<Plugs...>::instance()->template rm_cache<static_cast<size_t>(Rt)>(key);
+        }
+
+        template<DataType Rt,typename ...Args>
+        decltype(auto) rm_cache(Args&&... args)
+        {
+            using Ty = typename MapResPlug<static_cast<size_t>(Rt), Plugs...>::type;
+            
+            return rm_cache(std::make_tuple(std::forward<Args>(args)...));
+        }
+
+        template<DataType Rt>
+        decltype(auto) rm_cache_def()
+        {
+            using Ty = typename MapResPlug<static_cast<size_t>(Rt), Plugs...>::type;
+            static_assert(data_ck::has_default_args_func_vt<Ty>::value , "must has default_args function!!!");
+
+            return rm_cache(Ty::default_args());
+        }
+
+        inline static std::shared_ptr<DataMgr<Plugs...>> instance()
+        {
+#ifndef PF_ANDROID
+            if(!self) 
+                self = std::shared_ptr<DataMgr<Plugs...>>(new DataMgr<Plugs...>());
+#endif
+            return self;
+        }
+        template<typename T>
+        inline static std::shared_ptr<DataMgr<Plugs...>> create_instance(T&& t)
+        {
+            self = std::shared_ptr<DataMgr<Plugs...>>(new DataMgr<Plugs...>(std::forward<T>(t)));
+            return self;
+        }
+    protected:
+#ifdef PF_ANDROID
+        std::shared_ptr<EGLCxt> mgr;
+#endif
+
+private:    
+    inline static std::shared_ptr<DataMgr<Plugs...>> self;
+#ifndef PF_ANDROID
+        DataMgr<Plugs...>() {}
+#else
+        DataMgr<Plugs...>(std::shared_ptr<EGLCxt> mgr)
+        {
+            if(!mgr)
+                throw std::runtime_error("EGLCxt is nullptr!!!");
+            this->mgr = std::move(mgr);
+        }
+#endif
+    };
 } // namespace gld
