02f261bbb6ba0367857f12659a59cfacc72e6859
wws <1274704958@qq.com>
2020-03-27 16:56:10 +0800

    l.

--- example/geometry/main.cpp
+++ example/geometry/main.cpp
@@ -147,7 +147,7 @@ public:
         //glEnable(GL_BLEND);
         //glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-        glEnable(GL_CULL_FACE);
+        //glEnable(GL_CULL_FACE);
         glCullFace(GL_BACK);
         glEnable(GL_DEPTH_TEST);
         glDepthFunc(GL_LESS);
@@ -170,7 +170,7 @@ public:
             { 
                 auto mater = ptr->get_child(i)->get_comp<def::Material>();
                 if(mater)
-                    mater->uambient_strength = 0.36f;
+                    mater->uambient_strength = 0.2f;
                 st.push(ptr->get_child(i));
             }
         }
