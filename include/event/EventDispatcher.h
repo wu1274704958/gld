b99a9db634b2be2615e8f0b265af4ccfe86b0b24
wws <1274704958@qq.com>
2020-06-22 11:42:27 +0800

    event .

--- include/event/EventDispatcher.h
+++ include/event/EventDispatcher.h
@@ -7,6 +7,8 @@
 namespace evt {
 	template<typename TarTy>
 	struct EventDispatcher {
+
+		constexpr static size_t ReserveCount = 5;
 		
 		using TargetTy = TarTy;
 
@@ -22,35 +24,55 @@ namespace evt {
 
 			MouseEvent<TargetTy> ce(EventType::MouseDown, btn);
 
-			auto c = onMouse(x, y, ce,
+			std::tuple<EventHandler<TargetTy>*, glm::vec3, float> res[ReserveCount];
+
+			onMouse(x, y, ce,
 				[&ce](EventHandler<TargetTy>* c)->bool {
 				return c->handle_ty(EventType::MouseDown);
-			});
-			if (c)
+			},res,wws::arrLen(res));
+
+			for (auto i = 0; i < wws::arrLen(res); ++i)
 			{
-				ce.target = c->get_target();
-				c->handle_event(&ce);
+				if (std::get<0>(res[i]))
+				{
+					ce.pos = std::get<1>(res[i]);
+					ce.target = std::get<0>(res[i])->get_target();
+					if (std::get<0>(res[i])->handle_event(&ce))
+						break;
+				}
 			}
 		}
 		void onMouseUp(int btn, int mode, int x, int y)
 		{
 			MouseEvent<TargetTy> ce(EventType::MouseUp, btn);
 
-			auto c = onMouse(x,y,ce,
+			std::tuple<EventHandler<TargetTy>*, glm::vec3, float> res[ReserveCount];
+
+			onMouse(x,y,ce,
 				[&ce](EventHandler<TargetTy>* c)->bool {
 				return c->handle_ty(EventType::MouseUp);
-			});
-			if (c)
+			},res,wws::arrLen(res));
+
+			for (auto i = 0; i < wws::arrLen(res); ++i)
 			{
-				bool launch_click = c->last_type == EventType::MouseDown;
-
-				ce.target = c->get_target();
-				c->handle_event(&ce);
-
-				if (launch_click)
+				if (std::get<0>(res[i]))
 				{
-					ce.type = EventType::Click;
-					c->handle_event(&ce);
+					int f = 0;
+					ce.pos = std::get<1>(res[i]);
+					bool launch_click = std::get<0>(res[i])->last_type == EventType::MouseDown;
+
+					ce.target = std::get<0>(res[i])->get_target();
+					ce.type = EventType::MouseUp;
+					if (std::get<0>(res[i])->handle_event(&ce))
+						++f;
+
+					if (launch_click)
+					{
+						ce.type = EventType::Click;
+						if (std::get<0>(res[i])->handle_event(&ce))
+							++f;
+					}
+					if (f) break;
 				}
 			}
 			cache_btn = -1;
@@ -61,40 +83,55 @@ namespace evt {
 			{
 				MouseEvent<TargetTy> ce(EventType::MouseMove, cache_btn);
 
-				auto c = onMouse(x,y,ce,
+				std::tuple<EventHandler<TargetTy>*, glm::vec3, float> res[ReserveCount];
+
+				onMouse(x,y,ce,
 					[&ce](EventHandler<TargetTy>* c)->bool {
 					return c->handle_ty(EventType::MouseMove) &&
 						(c->last_type == EventType::MouseMove || c->last_type == EventType::MouseDown);
 				},
+					res,
+					wws::arrLen(res),
 					[&ce](EventHandler<TargetTy>* c) {
 					ce.type = EventType::MouseOut;
 					ce.target = c->get_target();
 					c->handle_event(&ce);
 				});
-				if (c)
+				
+				for (auto i = 0; i < wws::arrLen(res); ++i)
 				{
-					ce.type = EventType::MouseMove;
-					ce.target = c->get_target();
-					c->handle_event(&ce);
+					if (std::get<0>(res[i]))
+					{
+						ce.pos = std::get<1>(res[i]);
+						ce.type = EventType::MouseMove;
+						ce.target = std::get<0>(res[i])->get_target();
+						if (std::get<0>(res[i])->handle_event(&ce))
+							break;
+					}
 				}
 			}
 		}
 
-		EventHandler<TargetTy>* onMouse(int x,int y,MouseEvent<TargetTy>& ce,
+		void onMouse(int x,int y,MouseEvent<TargetTy>& ce,
 			std::function<bool(EventHandler<TargetTy>*)> into,
+			std::tuple<EventHandler<TargetTy>*,glm::vec3,float>* ehs,
+			size_t len,
 			std::function<void(EventHandler<TargetTy>*)> test_failed = std::function<void(EventHandler<TargetTy>*)>())
 		{
 			if (childlen_count() > 0)
 			{
+				for (auto i = 0; i < len; ++i)
+				{
+					std::get<0>(ehs[i]) = nullptr;
+					std::get<1>(ehs[i]) = glm::vec3(0.f,0.f,0.f);
+					std::get<2>(ehs[i]) = std::numeric_limits<float>::max();
+				}
+
 				glm::vec3 raypos, raydir;
 				mouse_ray(x, y, raypos, raydir);
 				
 				ce.raypos = raypos; ce.raydir = raydir; ce.camera_pos = camera_pos; ce.world = world;
 				ce.w_pos = glm::vec2(static_cast<float>(x), static_cast<float>(y));
-
-				float min_distance = std::numeric_limits<float>::max();
-				glm::vec3 curr_pos;
-				EventHandler<TargetTy>* curr = nullptr;
 
 				for (int i = 0; i < childlen_count(); ++i)
 				{
@@ -104,11 +141,17 @@ namespace evt {
 						if (c->onHandleMouseEvent(&ce) && glm::dot(glm::normalize(camera_dir), glm::normalize(ce.pos - camera_pos)) >= 0.f)
 						{
 							float dist = glm::length(ce.pos - camera_pos);
-							if (dist < min_distance)
+							for (auto j = 0; j < len; ++j)
 							{
-								curr_pos = ce.pos;
-								curr = c;
-								min_distance = dist;
+								if (dist < std::get<2>(ehs[j]))
+								{
+									if (j + 1 < len && std::get<0>(ehs[j]))
+										ehs[j + 1] = ehs[j];
+									std::get<1>(ehs[j]) = ce.pos;
+									std::get<0>(ehs[j]) = c;
+									std::get<2>(ehs[j]) = dist;
+									break;
+								}
 							}
 						}
 						else {
@@ -117,11 +160,7 @@ namespace evt {
 						}
 					}
 				}
-				if(curr)
-					ce.pos = curr_pos;
-				return curr;
 			}
-			return nullptr;
 		}
 
 		void mouse_ray(int x, int y,glm::vec3& raypos,glm::vec3& raydir)
