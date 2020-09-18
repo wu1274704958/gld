b4e27acf80074eccc2350dfba7680df47b56083a
wws <1274704958@qq.com>
2020-09-18 13:28:07 +0800

    music player win .

--- example/music_player/GetFileName.h
+++ example/music_player/GetFileName.h
@@ -13,8 +13,15 @@
 class GetFileName { 
 
 public:
-    static void getFileNameA(std::vector<MMFile> &v, const MMFILE_CHAR_TYPE *d);
-	static void getFileNameW(std::vector<MMFile> &v, const MMFILE_CHAR_TYPE *d);
+#ifdef PF_WIN32
+    static void getFileNameA(std::vector<MMFile>& v, const char* d);
+    static void getFileNameW(std::vector<MMFile>& v, const wchar_t* d);
+#else
+    static void getFileNameA(std::vector<MMFile>& v, const MMFILE_CHAR_TYPE* d);
+    static void getFileNameW(std::vector<MMFile>& v, const MMFILE_CHAR_TYPE* d);
+#endif // PF_WIN32
+
+    
 };
 
 
